c26e59bf305c02be3d3843f16adfd0f854a82eb5
Bernardo Santos <bernardosantos670@gmail.com>
2019-05-12 05:34:52 +0100

    Finished tp06

--- TP06/MyScene.js
+++ TP06/MyScene.js
@@ -10,6 +10,7 @@ class MyScene extends CGFscene {
         super.init(application);
         this.initCameras();
         this.initLights();
+        this.initMaterials();
 
         //Background color
         this.gl.clearColor(1.0, 1.0, 1.0, 1.0);
@@ -21,20 +22,43 @@ class MyScene extends CGFscene {
         this.enableTextures(true);
 
         //Objects connected to MyInterface
+        /*
         this.axiom = "F--F--F"; // "X"; //
         this.ruleF = "F+F--F+F"; // "FF"; //
-        //this.ruleX = "F[-X][X]F[-X]+FX";
+        this.ruleX = "F[-X][X]F[-X]+FX";
         this.angle = 60.0;
         this.iterations = 2;
         this.scaleFactor = 1;
-        this.lSystem = new MyLSystem(this);
+        */
+        
+        this.axiom = "X";
+        this.ruleF = "FF";
+        //this.ruleF = "F+F--F+F";
+        this.ruleX = "F[-X][X]F[-X]+FX";
+        this.angle = 30.0;
+        this.iterations = 4;
+        this.scaleFactor = 0.5;
+
+        this.lSystem = new MyLSPlant(this);
+
+        this.rulesX = [
+            "F[-X][X]F[-X]+X",
+            "F[-X][X]+X",
+            "F[+X]-X",
+            "F[/X][X]F[\\X]+X",
+            "F[\X][X]/X",
+            "F[/X]\X",
+            "F[^X][X]F[&X]^X",
+            "F[^X]&X",
+            "F[&X]^X"
+        ];        
 
         this.doGenerate = function () {
             this.lSystem.generate(
                 this.axiom,
                 {
                     "F": [ this.ruleF ],
-                    "X": [ this.ruleX ]
+                    "X": this.rulesX
                 },
                 this.angle,
                 this.iterations,
@@ -48,6 +72,44 @@ class MyScene extends CGFscene {
         //Initialize scene objects
         this.axis = new CGFaxis(this);
         
+    }
+
+    hexToRgbA(hex) {
+        var ret;
+        //either we receive a html/css color or a RGB vector
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            ret=[
+                parseInt(hex.substring(1,3),16).toPrecision()/255.0,
+                parseInt(hex.substring(3,5),16).toPrecision()/255.0,
+                parseInt(hex.substring(5,7),16).toPrecision()/255.0,
+                1.0
+            ];
+        }
+        else
+            ret=[
+                hex[0].toPrecision()/255.0,
+                hex[1].toPrecision()/255.0,
+                hex[2].toPrecision()/255.0,
+                1.0
+            ];
+        return ret;
+    }
+
+    initMaterials() {
+
+        let branch_color = this.hexToRgbA("#8b4513");
+        this.branch = new CGFappearance(this);
+        this.branch.setAmbient(branch_color[0], branch_color[1], branch_color[2], 1);
+        this.branch.setDiffuse(branch_color[0], branch_color[1], branch_color[2], 1);
+        this.branch.setSpecular(0.1, 0.1, 0.1, 1);
+        this.branch.setShininess(10.0);
+
+        let leaf_color = this.hexToRgbA("#027202");
+        this.leaf = new CGFappearance(this);
+        this.leaf.setAmbient(0.1/255, 0.145/255, 0.1/255, 1);
+        this.leaf.setDiffuse(leaf_color[0], leaf_color[1], leaf_color[2], 1);
+        this.leaf.setSpecular(0.1, 0.1, 0.1, 1);
+        this.leaf.setShininess(10.0);
     }
 
     initLights() {
