03466d9ca73936baf0eeb14256db91a421f85576
unknown <vhlg99@gmail.com>
2019-05-15 12:46:15 +0100

    Started MyBird.js

--- projB/MyBird.js
+++ projB/MyBird.js
@@ -12,6 +12,32 @@ class MyBird extends CGFobject {
     }
 
     initBuffers() {
+        this.head=new MyUnitCubeQuad(this.scene,0,0,0);
+        this.body=new MyUnitCubeQuad(this.scene,0,0,0);
+        this.wing1=new MyQuad(this.scene,1,1);
+        this.wing2=new MyQuad(this.scne,1,1);
+        this.nose= new MyPyramid(this.scene,10);
+    }
+
+    display(){
+
+    }
+
+    enableNormalViz() {
+        this.head.enableNormalViz();
+        this.body.enableNormalViz();
+        this.wing1.enableNormalViz();
+        this.wing2.enableNormalViz();
+        this.nose.enableNormalViz();
+        
+    }
+
+    disableNormalViz() {
+        this.head.disableNormalViz();
+        this.body.disableNormalViz();
+        this.wing1.enableNormalViz();
+        this.wing2.enableNormalViz();
+        this.nose.enableNormalViz();
         
     }
 }
