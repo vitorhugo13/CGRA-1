f0e0992b91530627e97550d613ee2e92b761b11e
unknown <vhlg99@gmail.com>
2019-05-22 12:28:00 +0100

    My terrain needed update, see ShaderScene TP5

--- projB/MyTerrain.js
+++ projB/MyTerrain.js
@@ -15,8 +15,13 @@ class MyTerrain extends CGFobject{
     initBuffers(){
         this.plane = new Plane(this,32);
 
+      
+    }
+
+    initMaterials(){
         this.terrainTex = new CGFtexture(this.scene, "images/terrain.jpg");
         this.terrainMap = new CGFtexture(this.scene, "images/heightmap.jpg");
+        this.terrainAltimetry = new CGFtexture(this.scene, "images/altimetry.png");
         
         this.terrainShader = new CGFshader(this.gl, "shaders/terrain.vert", "shaders/terrain.frag");
     }
